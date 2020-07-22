c6674a683524bc07cb4d790ae8ecf376ae59fd3d
Neisvestney <neisvestney113@gmail.com>
2020-07-22 12:08:17 +0300

    Initial commit

---
+++ SentToDo/Deskband.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CSDeskBand;
+using CSDeskBand.ContextMenu;
+
+namespace ExampleWpf
+{
+    [ComVisible(true)]
+    [Guid("AA01ACB3-6CCC-497C-9CE6-9211F2EDFC10")]
+    [CSDeskBandRegistration(Name = "SentToDo")]
+    public class Deskband : CSDeskBandWpf
+    {
+        public Deskband()
+        {
+            Options.ContextMenuItems = ContextMenuItems;
+            Options.MinHorizontalSize = new DeskBandSize(100, -1);
+        }
+
+        protected override UIElement UIElement => new UserControl1();
+
+        private List<DeskBandMenuItem> ContextMenuItems
+        {
+            get
+            {
+                var action = new DeskBandMenuAction("Action - Toggle submenu");
+                var separator = new DeskBandMenuSeparator();
+                var submenuAction = new DeskBandMenuAction("Submenu Action - Toggle checkmark");
+                var submenu = new DeskBandMenu("Submenu")
+                {
+                    Items = { submenuAction }
+                };
+
+                action.Clicked += (sender, args) => submenu.Enabled = !submenu.Enabled;
+                submenuAction.Clicked += (sender, args) => submenuAction.Checked = !submenuAction.Checked;
+
+                return new List<DeskBandMenuItem>() { action, separator, submenu };
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+﻿# SentToDo - Desktop
+Windows taskbar panel for displaying notes and tasks
+> :warning: **The program is still in development**
+
+![Alt text](images/taskbar%201.png?raw=true "Taskbar 1")
