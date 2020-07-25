cc6c828b9a508f21d52b63851e4f2b64e260f5b9
Neisvestney <neisvestney113@gmail.com>
2020-07-25 19:58:59 +0300

    Update installer

--- Installer/MainWindow.xaml.cs
+++ Installer/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Application = System.Windows.Application;
 
 namespace Installer
 {
@@ -15,6 +20,22 @@ namespace Installer
         private void Cancel_OnClick(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void Install_OnClick(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\Program Files";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                Install(dialog);
+            }
+        }
+        
+        private void Install(CommonOpenFileDialog dialog)
+        {
+            MessageBox.Show("You selected: " + dialog.FileName);
         }
     }
 }
