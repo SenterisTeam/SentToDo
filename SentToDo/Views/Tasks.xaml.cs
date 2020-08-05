f1e829c0a0fba8bbeaff6355706bde0072f7b16c
Xefise <Xefise@gmail.com>
2020-08-05 12:54:38 +0300

    Add WindowsOpener

--- SentToDo/Views/Tasks.xaml.cs
+++ SentToDo/Views/Tasks.xaml.cs
@@ -5,21 +5,9 @@ namespace SentToDo.Views
 {
     public partial class Tasks : Window
     {
-        static Tasks instance; // Singleton
-
         public Tasks()
         {
-            if (instance != null)
-            {
-                instance.Focus();
-                this.Close();
-            }
-            else
-            {
-                instance = this;
-                DataContext = new TaskVM();
-                InitializeComponent();
-            }
+            InitializeComponent();
         }
     }
 }
---
+++ SentToDo/WindowsOpener.cs
@@ -0,0 +1,48 @@
+﻿using SentToDo.Views;
+using System.Linq;
+using System.Windows;
+
+namespace SentToDo
+{
+    class WindowInfo
+    {
+        public string name;
+        public Window window;
+
+        public void Create(Window creatingWindow)
+        {
+            if (window != null)
+            {
+                window.Activate();
+                creatingWindow.Close();
+            }
+            else
+            {
+                window = creatingWindow;
+                window.Closing += RemoveInstance;
+            }
+        }
+
+        void RemoveInstance(object sender, System.ComponentModel.CancelEventArgs e) => window = null;
+    }
+
+    public static class WindowsOpener
+    {
+
+        static WindowInfo[] windows = new WindowInfo[] 
+        { 
+            new WindowInfo{ name = "tasks" }
+        };
+        public static void OpenWindow(string name)
+        {
+            Window window = null;
+            switch (name)
+            {
+                case "tasks":
+                    window = new Tasks();
+                    break;
+            }
+            if(window != null) windows.Where(w => w.name.Equals(name)).FirstOrDefault().Create(window);
+        }
+    }
+}
