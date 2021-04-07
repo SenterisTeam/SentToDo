fd03c42f91080f688edfdacdea615b889b6df18c
Xefise <Xefise@gmail.com>
2021-04-07 16:14:28 +0300

    Add SentToDo project

---
+++ SentToDo/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SentToDo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
